28821f443b9a563e101e9b77f16743e1a3f1a002
Ryan Gautier <ryangautier2@gmail.com>
2020-08-23 18:33:47 -0500

    move components into folders and add styling

--- server/index.js
+++ server/index.js
@@ -10,6 +10,13 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+  console.log("we have a new connection!!!");
+  socket.on('disconnect', () => {
+    console.log('user has left!!');
+  })
+})
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
